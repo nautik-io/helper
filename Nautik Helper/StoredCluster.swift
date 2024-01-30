7a4f2e64e2c86755cf1d5224e388b692a0e5ac2d
Elias Wilken <elias@nautik.io>
2024-01-30 20:42:42 +0100

    tweak exec process launching

--- Nautik Helper/StoredCluster.swift
+++ Nautik Helper/StoredCluster.swift
@@ -7,34 +7,34 @@ class StoredCluster: Codable, @unchecked Sendable {
     var keychain: Keychain.KeychainType
     var position: Double
     var name: String
-    
+
     var cluster: Cluster
     var authInfo: AuthInfo
-    
+
     var defaultNamespace: String
 
     var error: String?
-    
+
     var kubeConfigDeviceID: UUID
     var kubeConfigDeviceUser: String
     var kubeConfigPath: URL
     var kubeConfigContextName: String
     var credentialsExpireAt: Date?
     var lastEvaluation: Date
-    
+
     init(
         id: UUID,
         keychain: Keychain.KeychainType,
         position: Double,
         name: String,
-        
+
         cluster: Cluster,
         authInfo: AuthInfo,
-        
+
         defaultNamespace: String,
 
         error: String? = nil,
-        
+
         kubeConfigDeviceID: UUID,
         kubeConfigDeviceUser: String,
         kubeConfigPath: URL,
@@ -46,14 +46,14 @@ class StoredCluster: Codable, @unchecked Sendable {
         self.keychain = keychain
         self.position = position
         self.name = name
-        
+
         self.cluster = cluster
         self.authInfo = authInfo
-        
+
         self.defaultNamespace = defaultNamespace
 
         self.error = error
-        
+
         self.kubeConfigDeviceID = kubeConfigDeviceID
         self.kubeConfigDeviceUser = kubeConfigDeviceUser
         self.kubeConfigPath = kubeConfigPath
@@ -61,19 +61,19 @@ class StoredCluster: Codable, @unchecked Sendable {
         self.credentialsExpireAt = credentialsExpireAt
         self.lastEvaluation = lastEvaluation
     }
-    
+
     static let decoder = {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         return decoder
     }()
-    
+
     func evaluateAuth() async throws {
         if let caFile = cluster.certificateAuthority {
             let caData = try Data(contentsOf: URL(fileURLWithPath: caFile))
             cluster.certificateAuthorityData = caData
         }
-        
+
         if let tokenFile = authInfo.tokenFile {
             let token = try String(contentsOf: URL(fileURLWithPath: tokenFile), encoding: .utf8)
             authInfo.token = token
@@ -87,7 +87,7 @@ class StoredCluster: Codable, @unchecked Sendable {
             let clientKeyData = try Data(contentsOf: URL(fileURLWithPath: clientKeyFile))
             authInfo.clientKeyData = clientKeyData
         }
-        
+
         if let impersonate = authInfo.impersonate {
             // TODO
         }
@@ -97,7 +97,7 @@ class StoredCluster: Codable, @unchecked Sendable {
         if let impersonateUserExtra = authInfo.impersonateUserExtra {
             // TODO
         }
-        
+
         if let authProvider = authInfo.authProvider {
             // TODO
         }
@@ -107,16 +107,16 @@ class StoredCluster: Codable, @unchecked Sendable {
                 guard let stdout = try executeCommand(command: exec.command, arguments: exec.args) else {
                     throw "Executing \(exec.command) yielded no stdout."
                 }
-                
+
                 do {
                     let credential = try Self.decoder.decode(ExecCredential.self, from: Data(stdout.utf8))
-                    
+
                     await MainActor.run { [weak self] in
                         self?.authInfo.token = credential.status.token
-                        
+
                         self?.authInfo.clientCertificateData = credential.status.clientCertificateData.map { $0.data(using: .utf8).map { Data(base64Encoded: $0) } ?? nil } ?? nil
                         self?.authInfo.clientKeyData = credential.status.clientKeyData.map { $0.data(using: .utf8).map { Data(base64Encoded: $0) } ?? nil } ?? nil
-                        
+
                         self?.credentialsExpireAt = credential.status.expirationTimestamp
                     }
                 } catch {
@@ -130,7 +130,7 @@ class StoredCluster: Codable, @unchecked Sendable {
         } else {
             credentialsExpireAt = nil
         }
-        
+
         error = nil
         lastEvaluation = Date.now
     }
@@ -142,7 +142,7 @@ struct ExecCredential: Codable, @unchecked Sendable {
     let kind: String
     let spec: Spec
     let status: Status
-    
+
     struct Spec: Codable {
         let cluster: Cluster?
         let interactive: Bool?
@@ -160,7 +160,7 @@ func executeCommand(command: String, arguments: [String]? = nil) throws -> Strin
     guard let shell = try runProcess(command: "/usr/bin/env", arguments: ["/bin/sh", "-cl", "echo $SHELL"]) else {
         throw "Couldn't evaluate the user's SHELL."
     }
-    
+
     guard let cmdPath = try runProcess(command: "/usr/bin/env", arguments: [shell, "-cl\(shell.contains("zsh") ? "i" : "")", "which \(command)"]) else {
         throw "Executable \(command) not found in the user's PATH."
     }
@@ -168,10 +168,10 @@ func executeCommand(command: String, arguments: [String]? = nil) throws -> Strin
         throw "Executable \(command) not found in the user's PATH."
     }
 
-    let stdout = try runProcess(command: "/usr/bin/env", arguments: [shell, "-cl", "\(cmdPath) \(arguments.map { $0.joined(separator: " ") } ?? "")"])
+    let stdout = try runProcess(command: "/usr/bin/env", arguments: [shell, "-cl\(shell.contains("zsh") ? "i" : "")", "\(cmdPath) \(arguments.map { $0.joined(separator: " ") } ?? "")"])
 
     return stdout
-    
+
     func runProcess(command: String, arguments: [String]?, path: String? = nil) throws -> String? {
         let task = Process()
 
@@ -185,21 +185,29 @@ func executeCommand(command: String, arguments: [String]? = nil) throws -> Strin
             }
         }
 
-        task.launchPath = command
+        task.executableURL = URL(fileURLWithPath: command)
         if let arguments {
             task.arguments = arguments
         }
 
-        let pipe = Pipe()
-        task.standardOutput = pipe
-        task.standardError = pipe
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        task.standardOutput = outputPipe
+        task.standardError = errorPipe
 
-        task.launch()
-        task.waitUntilExit()
+        try task.run()
+        //task.waitUntilExit()
 
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let string = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+        
+        let output = String(decoding: outputData, as: UTF8.self).trimmingCharacters(in: .whitespacesAndNewlines)
+        let error = String(decoding: errorData, as: UTF8.self)
+        
+        if !error.isEmpty {
+            throw error
+        }
 
-        return string
+        return output
     }
 }
